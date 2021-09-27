0a961fc6d0ba8dfb62c3ac1816521c1383699548
Moira Leon <moira.leon2@gmail.com>
2021-09-27 01:52:56 -0600

    layout and homepage

---
+++ app/src/main/java/com/example/smart_washer_app/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.example.smart_washer_app;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
